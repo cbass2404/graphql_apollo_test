ecbe3b8ce85dbaaf5f4693b60d421451cd010fad
Cory Bass <cory.bass.07@gmail.com>
2021-06-09 15:41:37 -0500

    updated styling

---
+++ client/components/SongDetail.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class SongDetail extends Component() {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+    render() {
+        <div>
+            <h3>Song Detail</h3>
+        </div>;
+    }
+}
+
+export default SongDetail;
